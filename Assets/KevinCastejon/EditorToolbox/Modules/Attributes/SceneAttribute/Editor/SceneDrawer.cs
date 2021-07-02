dc3bccdb58b4b846550473c6c43b1b46505f5e27
Kevin Castejon <contact@kevincastejon.fr>
2021-07-02 18:21:27 +0200

    updated scene attribute

--- Assets/KevinCastejon/EditorToolbox/Modules/Attributes/SceneAttribute/Editor/SceneDrawer.cs
+++ Assets/KevinCastejon/EditorToolbox/Modules/Attributes/SceneAttribute/Editor/SceneDrawer.cs
@@ -17,18 +17,32 @@ namespace KevinCastejon.EditorToolbox
                 return;
             }
             List<string> scenes = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes).Select(x => ExtractSceneName(x.path)).ToList();
-            if (property.stringValue == "")
+            bool allowMissing = (attribute as SceneAttribute).allowMissing;
+            if (!scenes.Contains(property.stringValue))
             {
-                property.stringValue = scenes[0];
+                if (property.stringValue != "" && allowMissing)
+                {
+                    scenes.Insert(0, "<Missing>");
+                    string name = scenes[EditorGUI.Popup(position, label, 0, scenes.Select(x => new GUIContent(x)).ToArray())];
+                    property.stringValue = name == "<Missing>" ? property.stringValue : name;
+                }
+                else
+                {
+                    property.stringValue = scenes[0];
+                    property.stringValue = scenes[EditorGUI.Popup(position, label, 0, scenes.Select(x => new GUIContent(x)).ToArray())];
+                }
             }
-            property.stringValue = scenes[EditorGUI.Popup(position, label, scenes.IndexOf(property.stringValue), scenes.Select(x=>new GUIContent(x)).ToArray())];
+            else
+            {
+                property.stringValue = scenes[EditorGUI.Popup(position, label, scenes.IndexOf(property.stringValue), scenes.Select(x => new GUIContent(x)).ToArray())];
+            }
         }
 
         private string ExtractSceneName(string scenePath)
         {
             int indexOfLastSlash = scenePath.LastIndexOf("/") + 1;
             int indexOfExtension = scenePath.LastIndexOf(".unity") - indexOfLastSlash;
-            return(scenePath.Substring(indexOfLastSlash, indexOfExtension));
+            return (scenePath.Substring(indexOfLastSlash, indexOfExtension));
         }
     }
 }
